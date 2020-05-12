625a4f6c6f70f17956db8bcd77380e4983873a71
mtkramer <kramerm1@mail.usf.edu>
2020-05-12 15:33:26 -0400

    launch status 1.0

--- script.js
+++ script.js
@@ -33,17 +33,74 @@ function validateInput() {
    }
    else {
       event.preventDefault();
-      console.log("All inputs are good!");
+      id('fuelStatus').innerText = "Not enough fuel for journey";
+      id('fuelStatus').style.backgroundColor = 'red';
+      id('cargoStatus').innerText = "Too much cargo mass for lift off";
+      id('cargoStatus').style.backgroundColor = 'red';
+      return true;
    }
+
+   console.log("validateInput() error");
+   return false;
+
+}
+
+function updateLaunchStatus(ready) {
+   if (ready) {
+      id('launchStatus').innerText = "Shuttle ready for launch";
+      id('launchStatus').style.backgroundColor = 'green';
+   }
+   else {
+      id('launchStatus').innerText = "Shuttle not ready for launch";
+      id('launchStatus').style.backgroundColor = 'red';
+   }
+
+   id('faultyItems').style.visibility = 'visible';
 
 }
 
 window.addEventListener('DOMContentLoaded', () => {
    id('formSubmit').addEventListener('click', () => {
-      validateInput();
+
+      let fuelLevel;
+      let cargoMass;
+      let launchIsReady = false;
+      let fuelIsReady = false;
+      let cargoIsReady = false;
+
+      if (validateInput()) {
+         id('pilotStatus').innerText = `${id('pilotName').value} Ready`;
+         id('copilotStatus').innerText = `${id('copilotName').value} Ready`;
+         fuelLevel = Number(id('fuelLevel').value);
+         cargoMass = Number(id('cargoMass').value);
+      }
+
+      if (fuelLevel > 10000) {
+         id('fuelStatus').innerText = "Fuel level high enough for launch";
+         id('fuelStatus').style.backgroundColor = '#ECF0F1';
+         fuelIsReady = true;
+      }
+
+      if (cargoMass > 0 && cargoMass < 10000) {
+         id('cargoStatus').innerText = "Cargo mass low enough for launch";
+         id('cargoStatus').style.backgroundColor = '#ECF0F1';
+         cargoIsReady = true;
+      }
+
+      if (fuelIsReady && cargoIsReady) {
+         launchIsReady = true;
+      }
+
+      updateLaunchStatus(launchIsReady);
+
    });
 });
 
+/*
+If the user submits a cargo mass that is too large (more than 10,000 kilograms),
+change the list to visible with an updated cargo status stating that there is too much mass for the shuttle to take off.
+The text of launchStatus should also change to "Shuttle not ready for launch" and the color should change to red
+*/
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
